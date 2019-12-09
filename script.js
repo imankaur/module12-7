390603cb4b04d3689c31bf6082de574440a26ae6
imankaur <52762058+imankaur@users.noreply.github.com>
2019-12-08 18:15:27 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,83 @@
+let api_key='bujPhndNRqoU8ciylRfXIKdg59yarR7O6sqOsrWT';
+
+function callStateList(){
+  fetch(`https://developer.nps.gov/api/v1/parks?&api_key=${api_key}`)
+  .then(response => {
+    return response.json()
+  })
+  .then(allStatesResponse => {
+    loadDropDown(allStatesResponse)
+  })
+  .catch(error => console.log(error))
+}
+
+function callParksApi(stateSelected,numResults){
+  fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${stateSelected}&limit=${numResults-1}&api_key=bujPhndNRqoU8ciylRfXIKdg59yarR7O6sqOsrWT`)
+  .then(results =>
+    results.json())
+  .then(newResults =>
+    displayResults(newResults)
+  )
+  .catch(error => console.log(error))
+}
+
+function loadDropDown(allStates){
+  console.log(allStates);
+  $('.select-dropdown').empty();
+  for(let i = 0; i < allStates.data.length; i++){
+    let states = allStates.data[i].states;
+    console.log(states);
+
+    $('.select-dropdown').append(
+      `
+      <option class="dropdown-style" value="${states}">
+        ${states}
+      </option>
+      `
+      );
+  }
+  $(".select-dropdown").removeClass("hidden");
+}
+
+function submitButton(){
+  $('.form').submit(event => {
+    event.preventDefault();
+    let stateSelected = $('.select-dropdown :selected').val();
+    console.log(stateSelected);
+    let numResults = $('input[type=number]').val();
+    callParksApi(stateSelected, numResults);
+  })
+}
+
+function displayResults(newResults){
+  console.log(newResults);
+  $('.form').hide();
+  $('.results').empty();
+  for (let i = 0; i < newResults.data.length; i++){
+    let parkName = newResults.data[i].fullName;
+    console.log(parkName);
+
+    let parkDescription = newResults.data[i].description;
+    console.log(parkDescription);
+
+    let parkWebsite = newResults.data[i].url;
+    console.log(parkWebsite);
+
+    $('.results').append(
+      `
+      <h2>${parkName}</h2>
+      <p>${parkDescription}</p>
+      <div class="link">
+      <a href="${parkWebsite}" target="_blank">Visit Website</a>
+      </div>
+      `
+    );
+  }
+}
+
+function init() {
+  callStateList();
+	submitButton();
+}
+
+$(init);
